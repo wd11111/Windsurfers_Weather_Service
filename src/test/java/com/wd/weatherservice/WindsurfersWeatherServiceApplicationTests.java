e775c9962e5c31dbc2632467361708d2bfeaef87
Wojtek <xxwejtixx@gmail.com>
2023-02-26 21:56:00 +0100

    init commit

---
+++ src/test/java/com/wd/weatherservice/WindsurfersWeatherServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wd.weatherservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WindsurfersWeatherServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
