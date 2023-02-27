6f5138704dde1b9c8e2eed35210bc24405ca2420
Wojtek <xxwejtixx@gmail.com>
2023-02-27 10:53:44 +0100

    extracted logic to methods

--- src/main/java/com/wd/weatherservice/service/WeatherService.java
+++ src/main/java/com/wd/weatherservice/service/WeatherService.java
@@ -4,7 +4,6 @@ import com.wd.weatherservice.client.WeatherHttpClient;
 import com.wd.weatherservice.compatator.WeatherComparator;
 import com.wd.weatherservice.dto.SixteenDayForecastDto;
 import com.wd.weatherservice.dto.WeatherDataDto;
-import com.wd.weatherservice.exception.exception.NoSuitableLocationException;
 import com.wd.weatherservice.model.Forecast;
 import com.wd.weatherservice.model.Locations;
 import lombok.RequiredArgsConstructor;
@@ -31,22 +30,8 @@ public class WeatherService {
                 .toList();
 
         List<Forecast> forecasts = sixteenDayForecasts.stream()
-                .map(sixteenDayForecast -> {
-                    WeatherDataDto forecastForGivenDay = sixteenDayForecast.data()
-                            .stream()
-                            .filter(data -> data.datetime().equals(date))
-                            .toList()
-                            .get(0);
-
-                    return new Forecast(
-                            sixteenDayForecast.city_name(),
-                            sixteenDayForecast.country_code(),
-                            (forecastForGivenDay.min_temp() + forecastForGivenDay.max_temp()) / 2,
-                            forecastForGivenDay.wind_spd()
-                    );
-                })
-                .filter(forecast -> (forecast.windSpeed() >= MIN_WIND_SPEED && forecast.windSpeed() <= MAX_WIND_SPEED)
-                        && (forecast.avgTemp() >= MIN_TEMP && forecast.avgTemp() <= MAX_TEMP))
+                .map(sixteenDayForecast -> mapToForecast(date, sixteenDayForecast))
+                .filter(this::doesLocationFulfilCriteria)
                 .sorted(comparator)
                 .toList();
 
@@ -56,5 +41,26 @@ public class WeatherService {
                 .filter(forecast -> comparator.calculateValue(forecast) == highestCalculatedValue)
                 .toList();
     }
+
+    private boolean doesLocationFulfilCriteria(Forecast forecast) {
+        return (forecast.windSpeed() >= MIN_WIND_SPEED && forecast.windSpeed() <= MAX_WIND_SPEED)
+                && (forecast.avgTemp() >= MIN_TEMP && forecast.avgTemp() <= MAX_TEMP);
+    }
+
+    private Forecast mapToForecast(String date, SixteenDayForecastDto sixteenDayForecast) {
+        WeatherDataDto forecastForGivenDay = sixteenDayForecast.data()
+                .stream()
+                .filter(data -> data.datetime().equals(date))
+                .toList()
+                .get(0);
+
+        return new Forecast(
+                sixteenDayForecast.city_name(),
+                sixteenDayForecast.country_code(),
+                (forecastForGivenDay.min_temp() + forecastForGivenDay.max_temp()) / 2,
+                forecastForGivenDay.wind_spd()
+        );
+    }
+
 
 }
