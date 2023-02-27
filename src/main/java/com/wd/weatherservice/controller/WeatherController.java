12b4d353114e6d27e6ac79680d189d52f69be08a
Wojtek <xxwejtixx@gmail.com>
2023-02-27 10:59:29 +0100

    implemented controller

---
+++ src/main/java/com/wd/weatherservice/controller/WeatherController.java
@@ -0,0 +1,27 @@
+package com.wd.weatherservice.controller;
+
+import com.wd.weatherservice.dto.RequestDateDto;
+import com.wd.weatherservice.model.Forecast;
+import com.wd.weatherservice.service.WeatherService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/weather")
+@RequiredArgsConstructor
+public class WeatherController {
+
+    private final WeatherService weatherService;
+
+    @PostMapping
+    List<Forecast> getTheBestLocationForWindsurfing(@RequestBody @Valid RequestDateDto requestDateDto) {
+        return weatherService.getTheBestLocationForWindsurfing(requestDateDto.localDate().toString());
+    }
+
+}
