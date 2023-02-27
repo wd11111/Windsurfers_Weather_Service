2bffa5c361023ab2bdeecc45bb0e18e42469c5ce
Wojtek <xxwejtixx@gmail.com>
2023-02-27 12:40:47 +0100

    added static import

--- src/main/java/com/wd/weatherservice/validation/MaxDateValidator.java
+++ src/main/java/com/wd/weatherservice/validation/MaxDateValidator.java
@@ -6,11 +6,13 @@ import javax.validation.ConstraintValidatorContext;
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 
+import static java.time.LocalDate.*;
+
 public class MaxDateValidator implements ConstraintValidator<MaxDate, LocalDate> {
 
     @Override
     public boolean isValid(LocalDate localDate, ConstraintValidatorContext constraintValidatorContext) {
-        LocalDate furthestPossibleDate = LocalDate.now().plus(16, ChronoUnit.DAYS);
-        return localDate.isBefore(furthestPossibleDate) && (localDate.isEqual(LocalDate.now()) || localDate.isAfter(LocalDate.now()));
+        LocalDate furthestPossibleDate = now().plus(16, ChronoUnit.DAYS);
+        return localDate.isBefore(furthestPossibleDate) && (localDate.isEqual(now()) || localDate.isAfter(now()));
     }
 }
